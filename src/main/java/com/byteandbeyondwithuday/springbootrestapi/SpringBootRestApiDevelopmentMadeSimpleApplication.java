6f9cc0d789e4c6a86b2a9ce82da078c6b5634a5f
pateluday07 <pateluday07@gmail.com>
2025-04-05 20:19:21 +0530

    intial commit

---
+++ src/main/java/com/byteandbeyondwithuday/springbootrestapi/SpringBootRestApiDevelopmentMadeSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.byteandbeyondwithuday.springbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiDevelopmentMadeSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiDevelopmentMadeSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byteandbeyondwithuday/springbootrestapi/SpringBootRestApiDevelopmentMadeSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byteandbeyondwithuday.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiDevelopmentMadeSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
